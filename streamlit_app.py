da65986dcd5f19aea227260b61fd1a3e8005555d
Srivisak <68995308+Srivisak@users.noreply.github.com>
2026-06-01 15:36:18 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
 import os
 
 # Write directly to the app
@@ -13,7 +15,6 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your Smoothie will be: ", name_on_order)
 
-from snowflake.snowpark.functions import col
  
 #
 # option = st.selectbox(
@@ -23,7 +24,10 @@ from snowflake.snowpark.functions import col
 
 # st.write("You favourite fruit is:", option)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+## session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
